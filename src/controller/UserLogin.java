c30176108e7c009a9860cb32342e007c8443227c
unknown <daewoong.ko@gatech.edu>
2019-03-16 19:49:50 -0400

    Implement register button

--- src/controller/UserLogin.java
+++ src/controller/UserLogin.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -14,6 +15,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class UserLogin implements Initializable {
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -34,12 +36,11 @@ public class UserLogin implements Initializable {
     @FXML
     public void btnActionRegister1(ActionEvent event) {
         try {
+            Stage primaryStage = (Stage) ((Node) event.getSource()).getScene()
+                    .getWindow();
             Parent root = FXMLLoader.load(getClass()
                     .getResource("../view/Register_Navigation.fxml"));
-            Stage stage = new Stage();
-            stage.initStyle(StageStyle.DECORATED);
-            stage.setScene(new Scene(root));
-            stage.show();
+            primaryStage.setScene(new Scene(root));
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Cannot load User_Login.fxml");
