e36657d1d37179e3f302aceb23f4c5bc749e1c94
unknown <daewoong.ko@gatech.edu>
2019-03-17 00:52:51 -0400

    Implement btnActionUserOnly method in RegisterNavigation class

--- src/controller/RegisterNavigation.java
+++ src/controller/RegisterNavigation.java
@@ -1,7 +1,14 @@
 package controller;
 
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -10,5 +17,23 @@ public class RegisterNavigation implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    /**
+     * This is a method to get action from User Only button in
+     * Register_Navigation.fxml
+     * @param event Action from User Only button
+     */
+    public void btnActionUserOnly(Event event) {
+        try {
+            Stage primaryStage = (Stage) ((Node) event.getSource()).getScene()
+                    .getWindow();
+            Parent root = FXMLLoader.load(getClass()
+                    .getResource("../view/Register_User_Only.fxml"));
+            primaryStage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Cannot load Register_User_Only.xml");
+        }
     }
 }
