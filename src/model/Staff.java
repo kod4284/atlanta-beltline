55d2b1a23c1a0ad072067e2065c40d8589c4d422
kodw0402 <daewoong.ko@gatech.edu>
2019-04-01 21:02:53 -0400

    Add Staff class in model

---
+++ src/model/Staff.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Staff extends Employee {
+    private int numEventShifts;
+
+    public Staff(String firstName, String lastName, String password,
+                 ArrayList<String> email, String status, int emailCount,
+                 String address, String city, String state, int zipcode,
+                 int employee, String phone) {
+        super(firstName, lastName, password, email, status, emailCount,
+                address, city, state, zipcode, employee, phone);
+    }
+
+    public int getNumEventShifts() {
+        return numEventShifts;
+    }
+
+    public void setNumEventShifts(int numEventShifts) {
+        this.numEventShifts = numEventShifts;
+    }
+}
