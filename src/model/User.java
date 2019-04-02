efdae2382e4e9487640b5df37ec67068d91a8ca7
kodw0402 <daewoong.ko@gatech.edu>
2019-04-01 20:38:29 -0400

    Add User class

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,68 @@
 package model;
 
-public class User {
+public abstract class User {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+    private String status;
+    private int emailCount;
+
+    public User(String firstName, String lastName, String password,
+                String email, String status, int emailCount) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.email = email;
+        this.status = status;
+        this.emailCount = emailCount;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getEmailCount() {
+        return emailCount;
+    }
+
+    public void setEmailCount(int emailCount) {
+        this.emailCount = emailCount;
+    }
 }
