17611f15c7bb0ee8a2b2fc90039e9cbbf5f88096
kodw0402 <daewoong.ko@gatech.edu>
2019-04-01 20:57:13 -0400

    Add Manager class in model

--- src/model/Manager.java
+++ src/model/Manager.java
@@ -1,4 +1,12 @@
 package model;
 
-public class Manager {
+import java.util.ArrayList;
+
+public class Manager extends Employee {
+    public Manager(String firstName, String lastName, String password,
+                   ArrayList<String> email, String status, int emailCount,
+                   String address, String city, String state, int zipcode,
+                   int employee, String phone) {
+        super(firstName, lastName, password, email, status, emailCount, address, city, state, zipcode, employee, phone);
+    }
 }
