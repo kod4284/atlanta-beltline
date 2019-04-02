0fb800a6f1d0629c0f8d0e4df425bd07876c9221
kodw0402 <daewoong.ko@gatech.edu>
2019-04-01 21:11:50 -0400

    Add Visitor class in model

---
+++ src/model/Visitor.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Visitor extends User {
+    
+    private int myEventVisit;
+    private int mySiteVisit;
+
+    public Visitor(String firstName, String lastName, String password,
+                   ArrayList<String> email, String status, int emailCount) {
+        super(firstName, lastName, password, email, status, emailCount);
+    }
+
+    public int getMyEventVisit() {
+        return myEventVisit;
+    }
+
+    public void setMyEventVisit(int myEventVisit) {
+        this.myEventVisit = myEventVisit;
+    }
+
+    public int getMySiteVisit() {
+        return mySiteVisit;
+    }
+
+    public void setMySiteVisit(int mySiteVisit) {
+        this.mySiteVisit = mySiteVisit;
+    }
+}
