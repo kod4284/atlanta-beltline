2098a21ebbc774a327cb404bc4232c8ef035007d
Ray <woonglae@gmail.com>
2019-04-19 18:35:15 -0400

    connect Visitor Funcs views except take transit and transit history(user's view)

--- src/controller/VisitorTransit.java
+++ src/controller/VisitorTransit.java
@@ -1,4 +1,28 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class VisitorTransit {
+    @FXML
+    public void btnActionVisitorTransitDetailBack(ActionEvent event) {
+        try {
+            Stage primaryStage = (Stage) ((Node) event.getSource()).getScene()
+                    .getWindow();
+            Parent root = FXMLLoader.load(getClass()
+                    .getResource("../view/Visitor_Explore_Site.fxml"));
+            primaryStage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Cannot load User_Login.fxml");
+        }
+    }
+
 }
