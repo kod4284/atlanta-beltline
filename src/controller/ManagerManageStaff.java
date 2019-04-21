be6b6938057a21590e0755931ee37314413de8a5
Ray <woonglae@gmail.com>
2019-04-21 01:51:32 -0400

    add some variables

--- src/controller/ManagerManageStaff.java
+++ src/controller/ManagerManageStaff.java
@@ -2,11 +2,14 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import model.Session;
 
@@ -15,10 +18,23 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ManagerManageStaff implements Initializable {
+
+    @FXML ComboBox<String> site;
+    @FXML TextField firstName;
+    @FXML TextField lastName;
+    @FXML TextField startDate;
+    @FXML TextField endDate;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    @FXML
+    public void btnActionManagerManageStaffFilter(ActionEvent event) {
+
+    }
+
     public void btnActionBack(Event event) {
         if (Session.user.isEmployee() && Session.user.isManager()) {
             try {
