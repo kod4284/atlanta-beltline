7daa58cf1e255a591c8fba9160fe8d477c340acc
Ray <woonglae@gmail.com>
2019-04-21 03:58:52 -0400

    finish add variables of Views

--- src/controller/StaffEvent.java
+++ src/controller/StaffEvent.java
@@ -3,14 +3,36 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
-public class StaffEvent {
+import java.util.ResourceBundle;
+
+public class StaffEvent implements Initializable {
+
+    @FXML Label event;
+    @FXML Label site;
+    @FXML Label startDate;
+    @FXML Label endDate;
+    @FXML Label durationDays;
+    @FXML Label staffsAssigned;
+    @FXML Label capacity;
+    @FXML Label price;
+    @FXML Label description;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
     @FXML
     public void btnActionStaffEventDetailBack(ActionEvent event) {
         try {
