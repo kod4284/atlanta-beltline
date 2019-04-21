ae05146f3e65e65b8e8abb03ae2635fc9de79bc2
kodw0402 <daewoong.ko@gatech.edu>
2019-04-21 04:02:28 -0400

    Complete user take transit

--- src/controller/UserTakeTransit.java
+++ src/controller/UserTakeTransit.java
@@ -594,7 +594,7 @@ public class UserTakeTransit implements Initializable {
                 String sql = ("select distinct t1.transit_route, t1.transit_type, transit_price, site_number from\n" +
                         "(select transit_route, transit_type, transit_price\n" +
                         "from connect natural join transit\n" +
-                        "where transit_type=?\n" +
+                        "where site_name=?\n" +
                         "and transit_price between ? and ?\n" +
                         ") t1\n" +
                         "join\n" +
@@ -603,7 +603,7 @@ public class UserTakeTransit implements Initializable {
                         "group by transit_route, transit_type) t2\n" +
                         "on t1.transit_route=t2.transit_route and t1.transit_type=t2.transit_type\n");
                 PreparedStatement pst = conn.prepareStatement(sql);
-                pst.setString(1, transportType.getValue().toString());
+                pst.setString(1, containSite.getValue().toString());
                 pst.setDouble(2, Double.parseDouble(priceRangeStart.getText()));
                 pst.setDouble(3, Double.parseDouble(priceRangeEnd.getText()));
 
@@ -671,7 +671,98 @@ public class UserTakeTransit implements Initializable {
         transitTable.setItems(transitData);
     }
     private void Data_Data_NOT_NOT() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // create a connection to the database
+            Connection conn = DriverManager.getConnection(DB.url, DB.user, DB
+                    .password);
 
+            try {
+                //query
+
+                // sql statements
+                ArrayList<String> sites = new ArrayList<>();
+                //if no row return, go to catch
+                String sql = ("select distinct t1.transit_route, t1.transit_type, transit_price, site_number from\n" +
+                        "(select transit_route, transit_type, transit_price\n" +
+                        "from connect natural join transit\n" +
+                        "where site_name=?\n" +
+                        "and transit_type=?\n" +
+                        "and transit_price between ? and ?\n" +
+                        ") t1\n" +
+                        "join\n" +
+                        "(select transit_route, transit_type, count(*) as site_number\n" +
+                        "from connect natural join transit\n" +
+                        "group by transit_route, transit_type) t2\n" +
+                        "on t1.transit_route=t2.transit_route and t1.transit_type=t2.transit_type\n");
+                PreparedStatement pst = conn.prepareStatement(sql);
+                pst.setString(1, containSite.getValue().toString());
+                pst.setString(2, transportType.getValue().toString());
+                pst.setDouble(3, Double.parseDouble(priceRangeStart.getText()));
+                pst.setDouble(4, Double.parseDouble(priceRangeEnd.getText()));
+
+
+                ResultSet rs = pst.executeQuery();
+                while (rs.next()) {
+                    transitData.add(new UserTakeTransitData(new SimpleStringProperty(rs.getString
+                            ("transit_route")), new SimpleStringProperty(rs.getString
+                            ("transit_type")), rs.getDouble("transit_price")
+                            , rs.getInt("site_number")));
+                }
+                System.out.println(transitData);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            } finally {
+                if (conn != null) {
+                    conn.close();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        routeCol.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>
+                (param.getValue()));
+        routeCol.setCellFactory(param -> new TableCell<UserTakeTransitData,
+                UserTakeTransitData>() {
+            @Override
+            public void updateItem(UserTakeTransitData obj, boolean
+                    empty) {
+                super.updateItem(obj, empty);
+                if (empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    RadioButton radioButton = new RadioButton(obj.getRoute());
+                    radioButton.setToggleGroup(group);
+                    // Add Listeners if any
+                    setGraphic(radioButton);
+                    radioButton.setSelected(true);
+                    radioButton.setOnAction(new EventHandler<ActionEvent>() {
+
+                        @Override
+                        public void handle(ActionEvent arg0) {
+                            if (radioButton.isSelected()) {
+                                colIndex = getIndex();
+
+                            }
+
+                        }
+                    });
+                }
+            }
+            //private RadioButton radioButton = new RadioButton();
+
+
+        });
+        transportTypeCol.setCellValueFactory(new PropertyValueFactory<>
+                ("transportType"));
+        priceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
+        connectedSitesCol.setCellValueFactory(new PropertyValueFactory<>
+                ("connectedSites"));
+
+        transitTable.setItems(transitData);
     }
     public void btnActionFilter(ActionEvent event) {
         transitData = FXCollections.observableArrayList();
